8dd34769659cc2806a1a33b52a604403f803242a
ParkSungYil <shfur2006@gmail.com>
2022-06-21 23:53:22 +0900

    추가 : 프로젝트 Init

---
+++ src/main/java/com/triple/TripleSubject/TripleSubjectApplication.java
@@ -0,0 +1,13 @@
+package com.triple.TripleSubject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleSubjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleSubjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/triple/TripleSubject/TripleSubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triple.TripleSubject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleSubjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
