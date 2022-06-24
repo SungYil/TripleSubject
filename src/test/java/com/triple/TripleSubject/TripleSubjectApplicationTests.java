9af61fd3083657cb599c45caef665895dcd71166
ParkSungYil <shfur2006@gmail.com>
2022-06-25 00:04:47 +0900

    수정 : 리뷰 수정 시 state를 참조하도록 sql문 수정

--- src/test/java/com/triple/TripleSubject/TripleSubjectApplicationTests.java
+++ src/test/java/com/triple/TripleSubject/TripleSubjectApplicationTests.java
@@ -3,6 +3,7 @@ package com.triple.TripleSubject;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.jdi.request.EventRequest;
 import com.triple.TripleSubject.dtos.EventDto;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -44,7 +45,7 @@ class TripleSubjectApplicationTests {
 
 	@Test
 	void whenGetPointByUserId_thenOk()throws Exception{
-		var result=requestGet("/users/3ede0ef2-92b7-4817-a5f3-0c575361f123/points");
+		var result=requestGet("/users/3ede0ef2-92b7-4817-a5f3-0c575361f000/points");
 
 		result.andDo(print()).andExpect(status().isOk());
 	}
@@ -62,28 +63,30 @@ class TripleSubjectApplicationTests {
 	}
 
 	@Test
+	@Order(1)
 	void whenPostEventAdd_thenOk()throws Exception{
 		List<String> images=new ArrayList<>();
 		images.add("e4d1a64e-a531-46de-88d0-ff0ed70c0bb8");
 		images.add("afb0cef2-851d-4a50-bb07-9cc15cbdc332");
 		var result = requestPost(new EventDto("REVIEW",
 				"ADD",
-				"240a0658-dc5f-4878-9381-ebb7b2667715",
+				"240a0658-dc5f-4878-9381-ebb7b2667777",
 				"좋아요!",
 				images,
-				"3ede0ef2-92b7-4817-a5f3-0c575361f123",
+				"3ede0ef2-92b7-4817-a5f3-0c575361f000",
 				"2e4baf1c-5acb-4efb-a1af-eddada31b00f"),"/events");
 
 		result.andDo(print()).andExpect(status().isOk());
 	}
 	@Test
+	@Order(2)
 	void whenPostEventAddWithTypeError_thenError()throws Exception{
 		List<String> images=new ArrayList<>();
 		images.add("e4d1a64e-a531-46de-88d0-ff0ed70c0bb8");
 		images.add("afb0cef2-851d-4a50-bb07-9cc15cbdc332");
 		var result = requestPost(new EventDto("REVIEW",
 				"ADD",
-				"240a0658-dc5f-4878-9381-ebb7b2667712",
+				"240a0658-dc5f-4878-9381-123",
 				"좋아요!",
 				images,
 				"3ede0ef2-92b7-4817-a5f3-0c575361f745",
@@ -93,16 +96,29 @@ class TripleSubjectApplicationTests {
 	}
 
 	@Test
+	@Order(3)
 	void whenPostEventMod_thenOk()throws Exception{
 		List<String> images=new ArrayList<>();
+		images.add("e4d1a64e-a531-46de-88d0-ff0ed70c0bb8");
+		images.add("afb0cef2-851d-4a50-bb07-9cc15cbdc332");
+		var result = requestPost(new EventDto("REVIEW",
+				"ADD",
+				"240a0658-dc5f-4878-9381-ebb7b2667777",
+				"좋아요!",
+				images,
+				"3ede0ef2-92b7-4817-a5f3-0c575361f000",
+				"2e4baf1c-5acb-4efb-a1af-eddada31b00f"),"/events");
+		result.andDo(print());
+
+		images=new ArrayList<>();
 		//images.add("e4d1a64e-a531-46de-88d0-ff0ed70c0bb8");
 		//images.add("afb0cef2-851d-4a50-bb07-9cc15cbdc332");
-		var result = requestPost(new EventDto("REVIEW",
+		result = requestPost(new EventDto("REVIEW",
 				"MOD",
-				"240a0658-dc5f-4878-9381-ebb7b2667715",
+				"240a0658-dc5f-4878-9381-ebb7b2667777",
 				"123",
 				images,
-				"3ede0ef2-92b7-4817-a5f3-0c575361f123",
+				"3ede0ef2-92b7-4817-a5f3-0c575361f000",
 				"2e4baf1c-5acb-4efb-a1af-eddada31b00f"),"/events");
 
 		result.andDo(print()).andExpect(status().isOk());
@@ -114,7 +130,7 @@ class TripleSubjectApplicationTests {
 		images.add("afb0cef2-851d-4a50-bb07-9cc15cbdc332");
 		var result = requestPost(new EventDto("REVIEW",
 				"MOD",
-				"240a0658-dc5f-4878-9381-ebb7b2667772",
+				"240a0658-dc5f-4878-9381-123",
 				"123",
 				images,
 				"3ede0ef2-92b7-4817-a5f3-0c575361f745",
@@ -132,7 +148,7 @@ class TripleSubjectApplicationTests {
 				"240a0658-dc5f-4878-9381-ebb7b2667772",
 				"123",
 				images,
-				"3ede0ef2-92b7-4817-a5f3-0c575361f745",
+				"3ede0ef2-92b7-4817-a5f3-0c575361f700",
 				"2e4baf1c-5acb-4efb-a1af-eddada31b00f"),"/events");
 
 		result.andDo(print()).andExpect(status().isNotFound());
@@ -145,10 +161,10 @@ class TripleSubjectApplicationTests {
 		images.add("afb0cef2-851d-4a50-bb07-9cc15cbdc332");
 		var result = requestPost(new EventDto("REVIEW",
 				"DELETE",
-				"240a0658-dc5f-4878-9381-ebb7b2667715",
+				"240a0658-dc5f-4878-9381-ebb7b2667777",
 				"123",
 				images,
-				"3ede0ef2-92b7-4817-a5f3-0c575361f123",
+				"3ede0ef2-92b7-4817-a5f3-0c575361f000",
 				"2e4baf1c-5acb-4efb-a1af-eddada31b00f"),"/events");
 
 		result.andDo(print()).andExpect(status().isOk());
@@ -160,7 +176,7 @@ class TripleSubjectApplicationTests {
 		images.add("afb0cef2-851d-4a50-bb07-9cc15cbdc332");
 		var result = requestPost(new EventDto("REVIEW",
 				"DELETE",
-				"240a0658-dc5f-4878-9381-ebb7b2667714",
+				"240a0658-dc5f-4878-9381-ebb7b2667715",
 				"123",
 				images,
 				"3ede0ef2-92b7-4817-a5f3-0c575361f000",
@@ -176,7 +192,7 @@ class TripleSubjectApplicationTests {
 		images.add("afb0cef2-851d-4a50-bb07-9cc15cbdc332");
 		var result = requestPost(new EventDto("REVIEW",
 				"DELETE",
-				"240a0658-dc5f-4878-9381-ebb7b2667715",
+				"240a0658-dc5f-4878-9381-123",
 				"123",
 				images,
 				"3ede0ef2-92b7-4817-a5f3-0c575361f000",
